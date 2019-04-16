3832114db487c32a2428d793f2a30781931fb010
samuelsegal <Sbcrtmwsc()@101>
2019-04-16 11:46:49 -0400

    styled validation

--- ppm-react-app/src/components/Project/AddProject.js
+++ ppm-react-app/src/components/Project/AddProject.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import Proptyes from 'prop-types';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createProject } from '../../actions/projectActions';
+import classnames from 'classnames';
 
 class AddProject extends Component {
 	constructor() {
@@ -13,17 +14,25 @@ class AddProject extends Component {
 			description: '',
 			start_date: '',
 			end_date: '',
+			errors: {},
 		};
 
 		this.onChange = this.onChange.bind(this);
 		this.onSubmit = this.onSubmit.bind(this);
 	}
 
-	onChange = ({ target: { name, value } }) => {
-		this.setState({ [name]: value });
-	};
+	//life cycle hooks
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.errors) {
+			this.setState({ errors: nextProps.errors });
+		}
+	}
 
-	onSubmit = e => {
+	onChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
+	}
+
+	onSubmit(e) {
 		e.preventDefault();
 		const newProject = {
 			projectName: this.state.projectName,
@@ -32,24 +41,14 @@ class AddProject extends Component {
 			start_date: this.state.start_date,
 			end_date: this.state.end_date,
 		};
-		console.log(newProject);
 		this.props.createProject(newProject, this.props.history);
-	};
+	}
 
 	render() {
+		const { errors } = this.state;
+
 		return (
 			<div>
-				{
-					//check name attribute input fields
-					//create constructor
-					//set state
-					//set value on input fields
-					//create onChange function
-					//set onChange on each input field
-					//bind on constructor
-					//check state change in the react extension
-				}
-
 				<div className="project">
 					<div className="container">
 						<div className="row">
@@ -60,31 +59,46 @@ class AddProject extends Component {
 									<div className="form-group">
 										<input
 											type="text"
-											className="form-control form-control-lg "
+											className={classnames('form-control form-control-lg', {
+												'is-invalid': errors.projectName,
+											})}
 											placeholder="Project Name"
 											name="projectName"
 											value={this.state.projectName}
 											onChange={this.onChange}
 										/>
+										{errors.projectName && (
+											<div className="invalid-feedback">{errors.projectName}</div>
+										)}
 									</div>
 									<div className="form-group">
 										<input
 											type="text"
-											className="form-control form-control-lg"
+											className={classnames('form-control form-control-lg', {
+												'is-invalid': errors.projectIdentifier,
+											})}
 											placeholder="Unique Project ID"
 											name="projectIdentifier"
 											value={this.state.projectIdentifier}
 											onChange={this.onChange}
 										/>
+										{errors.projectIdentifier && (
+											<div className="invalid-feedback">{errors.projectIdentifier}</div>
+										)}
 									</div>
 									<div className="form-group">
 										<textarea
-											className="form-control form-control-lg"
+											className={classnames('form-control form-control-lg', {
+												'is-invalid': errors.description,
+											})}
 											placeholder="Project Description"
 											name="description"
 											value={this.state.description}
 											onChange={this.onChange}
 										/>
+										{errors.description && (
+											<div className="invalid-feedback">{errors.description}</div>
+										)}
 									</div>
 									<h6>Start Date</h6>
 									<div className="form-group">
@@ -119,9 +133,15 @@ class AddProject extends Component {
 }
 
 AddProject.propTypes = {
-	createProject: Proptyes.func.isRequired,
+	createProject: PropTypes.func.isRequired,
+	errors: PropTypes.object.isRequired,
 };
+
+const mapStateToProps = state => ({
+	errors: state.errors,
+});
+
 export default connect(
-	null,
+	mapStateToProps,
 	{ createProject }
 )(AddProject);
