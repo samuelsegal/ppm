62722a8f5ce4f7414d7db967986117f74a8ae3cd
samuelsegal <Sbcrtmwsc()@101>
2019-04-16 12:55:53 -0400

    getProjects populates projects list

--- ppm-react-app/src/components/Dashboard.js
+++ ppm-react-app/src/components/Dashboard.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import ProjectItem from './Project/ProjectItem';
 import CreateProjectButton from './Project/CreateProjectButton';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getProjects } from '../actions/projectActions';
 
 class Dashboard extends Component {
+	componentDidMount() {
+		this.props.getProjects();
+	}
+
 	render() {
+		const { projects } = this.props.project;
 		return (
 			<div className="projects">
 				<div className="container">
@@ -14,7 +22,9 @@ class Dashboard extends Component {
 							<CreateProjectButton />
 							<br />
 							<hr />
-							<ProjectItem />
+							{projects.map(project => (
+								<ProjectItem project={project} />
+							))}
 						</div>
 					</div>
 				</div>
@@ -22,5 +32,16 @@ class Dashboard extends Component {
 		);
 	}
 }
+Dashboard.protoTypes = {
+	project: PropTypes.object.isRequired,
+	getProjects: PropTypes.func.isRequired,
+};
 
-export default Dashboard;
+const mapStateToProps = state => ({
+	project: state.project,
+});
+
+export default connect(
+	mapStateToProps,
+	{ getProjects }
+)(Dashboard);
