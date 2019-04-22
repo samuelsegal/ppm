4c12eeb0f53d688fa7e47a8c533e762c8a13f3ac
samuelsegal <samuelmosessegal@gmail.com>
2019-04-22 11:53:29 -0400

    delete backend

---
+++ ppmservices/src/main/java/com/sms/ppm/services/impl/ProjectTaskServiceImpl.java
@@ -0,0 +1,95 @@
+package com.sms.ppm.services.impl;
+
+import com.github.dozermapper.core.Mapper;
+import com.sms.ppm.domain.Backlog;
+import com.sms.ppm.domain.Project;
+import com.sms.ppm.domain.ProjectTask;
+import com.sms.ppm.exceptions.ProjectNotFoundException;
+import com.sms.ppm.repositories.BacklogRepository;
+import com.sms.ppm.repositories.ProjectRepository;
+import com.sms.ppm.repositories.ProjectTaskRepository;
+import com.sms.ppm.util.Status;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@Slf4j
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private Mapper mapper;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+        try {
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            projectTask.setBacklog(backlog);
+            Integer backLogSequence = backlog.getPTSequence();
+            backlog.setPTSequence(++backLogSequence);
+            projectTask.setProjectSequence(backlog.getProjectIdentifier() + "-" + backLogSequence);
+            projectTask.setProjectIdentifier(backlog.getProjectIdentifier());
+
+            if (projectTask.getStatus() == null || "".equals(projectTask.getStatus())) {
+                projectTask.setStatus(Status.TODO.toString());
+            }
+            if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
+                projectTask.setPriority(3);
+            }
+            log.info("Adding projectTask: {}", projectTask);
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception e) {
+            log.error("Error adding project task: {}", e.getMessage());
+            throwProjectNotFoundException(projectIdentifier);
+        }
+        return null;
+    }
+
+    public Iterable<ProjectTask> findBacklogById(String id) {
+        Project project = projectRepository.findByProjectIdentifier(id);
+        if (project == null) {
+            throwProjectNotFoundException(id);
+        }
+        return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
+    }
+
+    public ProjectTask findPTByProjectSequence(String backlog_id, String pt_sequence) {
+
+
+        //make sure backlog exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+        if (backlog == null) {
+            throwProjectNotFoundException(backlog_id);
+        }
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_sequence);
+        if (projectTask == null) {
+            throw new ProjectNotFoundException("Project Task with sequence " + pt_sequence + " does not exist");
+        }
+
+        if(!projectTask.getProjectIdentifier().equals(backlog_id)){
+            throw new ProjectNotFoundException("Project task " + pt_sequence + " does not exist in project " + backlog_id);
+        }
+        return projectTask;
+    }
+
+    public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_sequence){
+        //waiting for reason to do this.going to revisit
+        //ProjectTask pt = findPTByProjectSequence(backlog_id, pt_sequence);
+        //pt = updatedTask;
+        return projectTaskRepository.save(updatedTask);
+    }
+    private void throwProjectNotFoundException(String id) {
+        throw new ProjectNotFoundException("Project with ID: " + id + " does not exist.");
+    }
+}
