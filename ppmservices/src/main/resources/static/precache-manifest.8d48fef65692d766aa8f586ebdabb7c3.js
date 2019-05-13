d2a6712796575a346b7d57395c6fada40a313287
samuelsegal <samuelmosessegal@gmail.com>
2019-05-12 21:00:00 -0400

    add static files from reactjs

---
+++ ppmservices/src/main/resources/static/precache-manifest.8d48fef65692d766aa8f586ebdabb7c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d31872afb6e849b72af3",
+    "url": "/static/js/main.dea192e3.chunk.js"
+  },
+  {
+    "revision": "a32604ecdeda91e4b397",
+    "url": "/static/js/2.b370fba1.chunk.js"
+  },
+  {
+    "revision": "d31872afb6e849b72af3",
+    "url": "/static/css/main.f7eee933.chunk.css"
+  },
+  {
+    "revision": "a32604ecdeda91e4b397",
+    "url": "/static/css/2.154541c0.chunk.css"
+  },
+  {
+    "revision": "e20b9c659452eaef19f16ecf6e20f2a5",
+    "url": "/index.html"
+  }
+];
